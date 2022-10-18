5423f0bcd1d7fe7a7c86f0c02770350b7d98d996
Khasbulat Abdullin <khas.2015@gmail.com>
2022-10-18 03:13:11 +0300

    скрипт проверки покрытия готов

--- project/client/connector/scripts/cov.sh
+++ project/client/connector/scripts/cov.sh
@@ -8,41 +8,38 @@ yellow='\033[103m'
 default='\033[0m'
 
 cd build || exit
-echo $PWD
 lcov -t "tests/connector_test" -o coverage.info -c -d lib/CMakeFiles/connector.dir
-
 (genhtml -o report coverage.info --output-directory coverage-report/) > "${LOG_TEST}"
-
-
-VAR=$(cat "${LOG_TEST}")
 cat "${LOG_TEST}"
 
-lines_pattern="lines......: "
-functions_pattern="functions..: "
-
-OK=true
+ok_lines=true
+functions=true
 
 lines=$(grep -Eo '\.\.\.: [0-9]{1,2}.[0-9]' ../test.log | grep -Eo '[0-9]{1,2}.[0-9]' $cov | grep -Eo '[0-9]{2}')
-functions=$(grep -Eo '\.\.: [0-9]{1,2}.[0-9]' ../test.log | grep -Eo '[0-9]{1,2}.[0-9]' $cov | grep -Eo '[0-9]{2}')
+functions=$(grep -Eo 's\.\.: [0-9]{1,2}.[0-9]' ../test.log | grep -Eo '[0-9]{1,2}.[0-9]' $cov | grep -Eo '[0-9]{2}')
 
-
-
-if [[ "$VAR" == *"$lines_pattern"["1"][[:digit:]].[[:digit:]]%* ]] || [[ "$VAR" == *"$lines_pattern"["$1"][[:digit:]].[[:digit:]]%* ]]; then
+if [[ $lines -ge $1 ]]; then
   printf "\t${green}%s${default}\n" "LINES_SUCCESS"
 else
   printf "\t${red}%s${default}\n" "LINES_FAILED"
-  OK=false
+  ok_lines=false
 fi
 
-if [[ "$VAR" == *"$functions_pattern"["1"][[:digit:]][[:digit:]].[[:digit:]]%* ]] || [[ "$VAR" == *"$functions_pattern"["$2"][[:digit:]].[0-9]%* ]]; then
+if [[ $functions -ge $2 ]]; then
   printf "\t${green}%s${default}\n" "FUNCTION_SUCCESS"
 else
   printf "\t${red}%s${default}\n" "FUNCTION_FAILED"
-  OK=false
+  ok_functions=false
 fi
 
-if ! ($OK) ; then
-    printf "\n\n\t${red}%s${default}\n" "COVERAGE TESTING FAILED"
+if ! ($ok_lines) || ! ($ok_functions) ; then
+    printf "\n\t${red}%s${default}\n" "COVERAGE TESTING FAILED"
+    if ! ($ok_lines) ; then
+      printf "\n\t%s${red}%d%s${default}%s${green}%d%s${default}\n" "Lines coverage is " "$lines" "%" ", but expected: " "$1" "%"
+    fi
+    if ! ($ok_functions) ; then
+          printf "\n\t%s${red}%d%s${default}%s${green}%d%s${default}\n" "Functions coverage is " "$functions" "%" ", but expected: " "$2" "%"
+        fi
     exit 1
 else
     printf "\n\n\t${green}%s${default}\n" "COVERAGE TESTING PASSED"
